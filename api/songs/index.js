fb94cdc846995d0e355a7473f3b2977355892b59
schneidz1636 <schneidz@oregonstate.edu>
2018-06-06 00:27:30 -0700

    Initial songs commit with branch

---
+++ api/songs/index.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+
+// ROUTE: /songs
+// PARAMS:
+// QUERIES: 
+router.get('/', (req, res) => {
+
+});
+
+
+// ROUTE: /songs/songID
+// PARAMS:
+// QUERIES: 
+router.get('/:songID', (req, res) => {
+
+});
+
+
+
+
+
+exports.router = router;
