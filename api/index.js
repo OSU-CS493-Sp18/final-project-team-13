dfd5ac2c2758860ff131dec4ee479158d55f3be9
danielc6754 <choimi@oregonstate.edu>
2018-06-08 05:54:04 -0700

    reworked routing

--- api/index.js
+++ api/index.js
@@ -6,6 +6,18 @@ const { router: playlistsRouter } = require('./playlists');
 const { router: songsRouter } = require('./songs');
 const { router: artistsRouter } = require('./artists');
 
+router.get('/users/:userID/:playlistID', function(req, res, next) {
+	
+	if(req.method === 'GET') {
+		router.use('/:playlistID', playlistsRouter);
+	}
+	
+	if(req.params.userID && req.params.playlistID ) {
+		router.use('/users/:userID/:playlistID', playlistsRouter);
+	}
+	next();
+});
+
 router.use('/users', usersRouter);
 router.use('/genres', genresRouter);
 router.use('/playlists', playlistsRouter);
--- api/playlists/index.js
+++ api/playlists/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const validation = require('../../lib/validation');
 
+const { requireAuthentication } = require('../../lib/auth');
+
 /*
  * Schema for playlist object.
  */
@@ -96,24 +98,41 @@ router.get('/', function (req, res) {
     });
 });
 
-/*
- * Function to fetch all the playlists owned by a specific user.
- */
-function getPlaylistsByOwnerID(userID, mysqlPool) {
+function getPlaylistByID(playlistID, mysqlPool) {
   return new Promise((resolve, reject) => {
-    mysqlPool.query(
-      'SELECT * FROM playlists WHERE userid = ?',
-      [ userID ],
-      function (err, results) {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(results);
-        }
+    mysqlPool.query('SELECT * FROM playlists WHERE id = ?', [playlistID], function (err, results) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(results[0]);
       }
-    );
-  });
+    });
+  })
 }
+
+/* 
+ * ROUTE: /:playlistID
+ * PARAMS: req, res
+ * QUERIES: select all playlists 
+ */
+router.get('/:playlistID', function (req, res) {
+	const mysqlPool = req.app.locals.mysqlPool;
+	const playlistID = parseInt(req.params.playlistID);
+	getPlaylistByID(playlistID, mysqlPool)
+		.then((playlist) => {
+			if (playlist) {
+				res.status(200).json(playlist);
+			} else {
+				next();
+			}
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json({
+				error: "Failed to fetch playlist."
+			});
+		});
+});
 
 /*
  * MySQL function to insert new playlist
@@ -137,6 +156,41 @@ function insertNewPlaylist(playlist, mysqlPool) {
 }
 
 /*
+ * Route to create a new playlist.
+ */
+router.post('/:userID/playlists', requireAuthentication, (req, res) => {
+  	const mysqlPool = req.app.locals.mysqlPool;
+	if (req.user !== req.params.userID) {
+		res.status(403).json({
+			error: "Unauthorized to access that resource"
+		});
+	} else {
+		req.body.userid = req.user;
+		console.log(req.body);
+		if (validation.validateAgainstSchema(req.body, playlistSchema)) {
+			insertNewPlaylist(req.body, mysqlPool)
+				.then((id) => {
+					res.status(201).json({
+						id: id,
+					links: {
+						playlist: `/playlists/${id}`
+					}
+				});
+			})
+			.catch((err) => {
+				res.status(500).json({
+					error: "Error inserting playlist into DB.  Please try again later."
+				});
+			});
+		} else {
+			res.status(400).json({
+				error: "Request body is not a valid playlist object."
+			});
+		}
+	}
+});
+
+/*
  * MySQL function to update a playlist
  */
 function replacePlaylistByID(playlistID, playlist, mysqlPool) {
@@ -151,6 +205,45 @@ function replacePlaylistByID(playlistID, playlist, mysqlPool) {
     });
   });
 }
+
+/*
+ * Route to update a playlist.
+ */
+router.put('/:userID/:playlistID', requireAuthentication, (req, res) => {
+  	const mysqlPool = req.app.locals.mysqlPool;
+	if (req.user !== req.params.userID) {
+		res.status(403).json({
+			error: "Unauthorized to access that resource"
+		});
+	} else {
+		const playlistID = parseInt(req.params.playlistID);
+		req.body.userid = req.user;
+		if (validation.validateAgainstSchema(req.body, playlistSchema)) {
+			replacePlaylistByID(playlistID, req.body, mysqlPool)
+				.then((updateSuccessful) => {
+					if (updateSuccessful) {
+						res.status(200).json({
+							links: {
+								playlists: `/playlists/${playlistID}`
+							}
+						});
+					} else {
+						next();
+					}
+				})
+				.catch((err) => {
+					console.log(err);
+					res.status(500).json({
+						error: "Unable to update specified playlist.  Please try again later."
+					});
+				});
+		} else {
+			res.status(400).json({
+				error: "Request body is not a valid playlist object"
+			});
+		}
+	}
+});
 
 /*
  * MySQL function to delete a playlist
@@ -168,9 +261,77 @@ function deletePlaylistByID(playlistID, mysqlPool) {
 
 }
 
+/*
+ * Route to delete a playlist.
+ */
+router.delete('/:userID/:playlistID', requireAuthentication, (req, res) => {
+  	const mysqlPool = req.app.locals.mysqlPool;
+	if (req.user !== req.params.userID) {
+		res.status(403).json({
+			error: "Unauthorized to access that resource"
+		});
+	} else {
+		const playlistID = parseInt(req.params.playlistID);
+		deletePlaylistByID(playlistID, mysqlPool)
+			.then((deleteSuccessful) => {
+				if (deleteSuccessful) {
+					res.status(204).end();
+				} else {
+					next();
+				}
+			})
+			.catch((err) => {
+				res.status(500).json({
+					error: "Unable to delete playlist.  Please try again later."
+				});
+			});
+	}
+});
+
+/*
+ * Function to fetch all the playlists owned by a specific user.
+ */
+function getPlaylistsByOwnerID(userID, mysqlPool) {
+  return new Promise((resolve, reject) => {
+    mysqlPool.query(
+      'SELECT * FROM playlists WHERE userid = ?',
+      [ userID ],
+      function (err, results) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(results);
+        }
+      }
+    );
+  });
+}
+
+/*
+ * Route to list all of a user's playlists.
+ */
+router.get('/:userID/playlists', requireAuthentication, (req, res) => {
+	const mysqlPool = req.app.locals.mysqlPool;
+	if (req.user !== req.params.userID) {
+		res.status(403).json({
+			error: "Unauthorized to access that resource"
+		});
+	} else {
+		const userID = parseInt(req.params.userID);
+		getPlaylistsByOwnerID(userID, mysqlPool)
+			.then((playlists) => {
+				if (playlists) {
+					res.status(200).json({ playlists: playlists });
+				} else {
+					next();
+				}
+			})
+			.catch((err) => {
+				res.status(500).json({
+					error: "Unable to fetch playlists.  Please try again later."
+				});
+			});
+	}
+});
+
 exports.router = router;
-exports.playlistSchema = playlistSchema;
-exports.getPlaylistsByOwnerID = getPlaylistsByOwnerID;
-exports.insertNewPlaylist = insertNewPlaylist;
-exports.replacePlaylistByID = replacePlaylistByID;
-exports.deletePlaylistByID = deletePlaylistByID;
